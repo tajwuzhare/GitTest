4b6319504abb354306f8b9da9f1b6fe3400c48b4
tux <tajwuzhare@yahoo.com>
2012-12-26 19:18:46 -0500

    test commit

---
+++ src/MainClass.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: heisenberg
+ * Date: 12/26/12
+ * Time: 7:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainClass {
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+}
